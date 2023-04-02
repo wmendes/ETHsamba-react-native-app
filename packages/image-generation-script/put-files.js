279e17524796474f7014d22307ea3389f6832b6a
Wlademyr Mendes <wlademyr.mendes@gmail.com>
2023-04-02 05:49:16 -0300

    fix: fixing mint on script

--- packages/image-generation-script/put-files.js
+++ packages/image-generation-script/put-files.js
@@ -5,13 +5,20 @@ import axios from 'axios';
 import download from 'download';
 import { nanoid } from 'nanoid'
 import * as dotenv from 'dotenv'
+import * as Pinata from '@pinata/sdk';
+import dataNft from './nft.json' assert {type: 'json'};
 
-import pinata from '@pinata/sdk'
-const pinataObj = new pinataSDK('de79075c9aa850920020', '4d00ae7c1b1faec14f8981f1ad3e8f4a06a426cad19f4e165dda45dcf0e4a58a');
-
+import Web3 from "web3";
+import { newKitFromWeb3 } from "@celo/contractkit";
 
 async function main () {
   dotenv.config()
+
+const web3 = new Web3("https://alfajores-forno.celo-testnet.org");
+const kit = newKitFromWeb3(web3);
+
+const contract = new kit.web3.eth.Contract(dataNft.abi, "0x3e0eaFf16144649b43EeADfbc9EA69F63B8D6B83")
+
 
   const weather = {
     1 : "clear sky",
@@ -33,7 +40,8 @@ async function main () {
 }  
 
   let date_ob = new Date();
-  console.log(date_ob.toISOString());
+  const dayTime = date_ob.getHours() > 6 && date_ob.getHours() < 20 ? " by day" : " by night";
+
   const weatherdata = await axios.get('https://api.meteomatics.com/' + date_ob.toISOString() + '/weather_symbol_1h:idx/-22.912853606173396,-43.17995910299403/json',
     {
       auth: {
@@ -52,7 +60,7 @@ async function main () {
 
   const { data } = await axios.post('https://api.openai.com/v1/images/generations', 
   {
-    "prompt": "arcos da lapa with " + weather[intcode],
+    "prompt": "arcos da lapa with " + weather[intcode] + dayTime,
     "n": 1,
     "size": "512x512" 
   }, {
@@ -61,24 +69,36 @@ async function main () {
       'Authorization': 'Bearer ' + process.env.DALLE_KEY
     }
   });
+  
+
+  const token = process.env.WEB3_KEY;
+  const storage = new Web3Storage({ token })
 
   let filename = nanoid() + '.jpg';
   await download(data.data[0].url, 'temp', {'filename' : filename});
-  const readableStreamForFile = fs.createReadStream(filename);
+  const pathFiles = await getFilesFromPath('temp/' + filename);
+  const cid = await storage.put(pathFiles);
+  const ipfsImageFile = cid + "/" + filename;
   
-  pinata.pinFileToIPFS(readableStreamForFile, options).then((result) => {
-    //handle results here
-    console.log(result);
-  }).catch((err) => {
-      //handle error here
-      console.log(err);
+  // let metaObject = {
+  //   "name": "Mine",
+  //   "description": "Mine inseda a tiny mount",
+  //   "image": "https://gateway.pinata.cloud/ipfs/" + ipfsImageFile
+  // }
+
+  contract.methods.mint('0x3e0eaFf16144649b43EeADfbc9EA69F63B8D6B83', ipfsImageFile)
+  .send({ from: '0x3e0eaFf16144649b43EeADfbc9EA69F63B8D6B83'})
+  .on('transactionHash', function (hash) {
+    console.log(hash);
+  })
+  .on('receipt', function (receipt) {
+      console.log(receipt);
+  })
+  .on('error', function (error, receipt) {
+      console.log(error);
+      console.log(receipt);
   });
 
-
-  // const token = process.env.WEB3_KEY;
-
-  // const storage = new Web3Storage({ token })
-  // const files = []
 
 }
 
